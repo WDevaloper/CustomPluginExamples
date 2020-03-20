1e4d2b834533713e48cdde87829cd734bbe37965
wangfayouy <f_y_wang6@163.com>
2020-03-20 15:17:30 +0800

    兼容kotlin

--- app/src/main/java/com/github/plugin/exalple/test/MainComponentKt.kt
+++ app/src/main/java/com/github/plugin/exalple/test/MainComponentKt.kt
@@ -6,6 +6,8 @@ import android.util.Log
 
 import com.github.plugin.common.IComponent
 
+
+//兼容kotlin
 class MainComponentKt : IComponent {
     override fun attachBaseContext(base: Context) {
         Log.e("tag", "MainComponentKt for attachBaseContext method")
