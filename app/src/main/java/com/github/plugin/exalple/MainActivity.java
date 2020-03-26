c6f4bce5cfa261ffd2243391554a0c5b64cd1d5b
wangfayouy <f_y_wang6@163.com>
2020-03-26 17:33:32 +0800

    update

--- app/src/main/java/com/github/plugin/exalple/MainActivity.java
+++ app/src/main/java/com/github/plugin/exalple/MainActivity.java
@@ -3,7 +3,10 @@ package com.github.plugin.exalple;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.github.plugin.exalple.test.TestInjejct;
 
@@ -18,8 +21,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new TestInjejct().test();
-
-
     }
 
 
@@ -27,5 +28,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.e("tag", "onDestroy>>>" + isFinishing());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("tag", "onPause>>>" + isFinishing());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("tag", "onStop>>>" + isFinishing());
+    }
+
+    public void toMain2Activity(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
